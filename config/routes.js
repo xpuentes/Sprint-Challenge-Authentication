0ac186b231d5519575015756fa9df843a814adc2
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2019-04-05 20:56:07 -0500

    Implemented user login and registration; login returning token

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,10 @@
 const axios = require('axios');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const { authenticate } = require('../auth/authenticate');
+const db = require('../database/dbConfig');
+
+const { authenticate, jwtKey } = require('../auth/authenticate');
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -10,10 +14,42 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  db('users')
+    .insert(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 }
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+  
+  db('users')
+    .where('username', username)
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          token,
+          message: `You may pass!`,
+        });
+      } else {
+        res.status(401).json({ message: 'You shall not PASS!' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+
 }
 
 function getJokes(req, res) {
@@ -30,3 +66,16 @@ function getJokes(req, res) {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
 }
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+
+  const options = {
+    expiresIn: '1d'
+  };
+
+  return jwt.sign(payload, jwtKey, options);
+};
